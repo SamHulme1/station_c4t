5badc1496eddd28fa4c95712ed4e2d7aef28024c
SamHulme1 <samhulmephotography@gmail.com>
2022-11-23 20:56:16 +0000

    created static files , added and styled images for characters and styled their descriptions

---
+++ static/js/scripts.js
@@ -0,0 +1,13 @@
+
+  let collapse = document.getElementsByClassName("collapse");
+  for (let i = 0; i < collapse.length; i++) {
+    collapse[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      let content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
