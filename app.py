a1dad91f4d64273caf05da9d2f9e571f5fa2bdc4
SamHulme1 <samhulmephotography@gmail.com>
2022-11-28 16:58:32 +0000

    updated signup route so that users can create accounts

--- app.py
+++ app.py
@@ -24,12 +24,35 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def create_account():
     """Signup form"""
     form = signup()
-    if form.validate_on_submit():
-        return redirect(url_for("successful"))
+    # form signup form from forms
+    if request.method == "POST":
+        existing_captain = mongo.db.users.find_one(
+            {"username": request.form["username"]})
+    # if method is post search db to see if a
+    # name is already on the db with the same name
+        if existing_captain:
+            flash(f"The user:{existing_captain} already exists")
+            return redirect(url_for("create_account"))
+    # if name already exists return the users
+    #  to the create account page to try again
+        if existing_captain is None:
+            password = request.form["password"]
+            mongo.db.users.insert_one({
+                "username": request.form["username"],
+                "password": generate_password_hash(request.form["password"])
+            })
+    # if the user doesn't exist create
+    #  a new user on the db using the form credentials
+
+            session["user"] = request.form.get("username")
+            flash("account created sucessfully")
+            return redirect(url_for("index"))
+    # store the user in local storage
+
     return render_template(
         "signup.html",
         form=form,
@@ -38,6 +61,7 @@ def create_account():
 
 @app.route("/get_citizens")
 def get_citizens():
+    # get the citizens stored in the db
     citizens = mongo.db.citizens.find()
     return render_template(
         "citizens.html", citizens=citizens, page_title="citizens")
@@ -45,6 +69,7 @@ def get_citizens():
 
 @app.route("/get_ships")
 def get_ships():
+    # get the ships stored in the db
     ships = mongo.db.ships.find()
     return render_template(
         "ships.html", ships=ships, page_title="ships")
