c68740a17d54c1246cd4b2084439b3666f284c81
SamHulme1 <samhulmephotography@gmail.com>
2022-11-28 22:45:06 +0000

    created form for logining in, form can be logged in via a normal account or through a seperate user type called admins which is stored on a seperate db section

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from forms import signup
+from forms import signup, login_to_account
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
@@ -40,21 +40,60 @@ def create_account():
     # if name already exists return the users
     #  to the create account page to try again
         if existing_captain is None:
-            password = request.form["password"]
             mongo.db.users.insert_one({
                 "username": request.form["username"],
                 "password": generate_password_hash(request.form["password"])
             })
     # if the user doesn't exist create
     #  a new user on the db using the form credentials
-
-            session["user"] = request.form.get("username")
+            session["user"] = request.form["username"]
             flash("account created sucessfully")
             return redirect(url_for("index"))
     # store the user in local storage
 
     return render_template(
         "signup.html",
+        form=form,
+    )
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = login_to_account()
+    if request.method == "POST":
+        existing_captain = mongo.db.users.find_one(
+            {"username": request.form["username"]})
+        admin = mongo.db.admins.find_one(
+            {"username": request.form["username"],
+                "password": request.form["password"]})
+
+    # if method is post search db to see if a
+    # name is already on the db with the same name
+        if existing_captain:
+            # check if existinging captin has the correct password to login
+            if check_password_hash(
+                    existing_captain["password"], request.form["password"]):
+                # store user in local storage
+                # display message to user after login
+                session["user"] = request.form["username"]
+                flash(f"Welcome back captain")
+            else:
+                flash(
+                    "Unauthorized password/username, please try again")
+                # if user isn't in database tell the user they're unathorized
+                return redirect(url_for("login"))
+
+        elif admin:
+            # if user is admin log in as admin
+            # store user in local storage
+            session["user"] = request.form["username"]
+            flash("Welcome admiral")
+        else:
+            flash("Unauthorized password/username")
+            return redirect(url_for("login"))
+
+    return render_template(
+        "login.html",
         form=form,
     )
 
