b20e82e4d5f14231e2ebf5e057846489bce934cf
SamHulme1 <samhulmephotography@gmail.com>
2022-12-15 11:37:04 +0000

    changed debug to false

--- app.py
+++ app.py
@@ -238,4 +238,4 @@ def not_found(error):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
